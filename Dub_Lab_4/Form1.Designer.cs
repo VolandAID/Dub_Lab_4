4e82d659dd6f6f898b0fd031352401d8f5942c91
Anton <55982537+VolandAID@users.noreply.github.com>
2020-01-09 15:28:37 +0300

    Add files via upload

---
+++ Dub_Lab_4/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dub_Lab_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxFind = new System.Windows.Forms.TextBox();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBoxFileReadTime = new System.Windows.Forms.TextBox();
+            this.textBoxFileReadCount = new System.Windows.Forms.TextBox();
+            this.textBoxExactTime = new System.Windows.Forms.TextBox();
+            this.listBoxResult = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(40, 498);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Чтение из файла";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(507, 104);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(140, 29);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Поиск";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(181, 52);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Время чтения из файла:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(181, 78);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(263, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Количество уникальных слов в файле:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(181, 110);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Слово для поиска:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textBoxFind
+            // 
+            this.textBoxFind.Location = new System.Drawing.Point(324, 108);
+            this.textBoxFind.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxFind.Name = "textBoxFind";
+            this.textBoxFind.Size = new System.Drawing.Size(172, 22);
+            this.textBoxFind.TabIndex = 6;
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Location = new System.Drawing.Point(536, 501);
+            this.buttonClose.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.buttonClose.Size = new System.Drawing.Size(105, 34);
+            this.buttonClose.TabIndex = 8;
+            this.buttonClose.Text = "Выход";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click_1);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(181, 155);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Время поиска:";
+            // 
+            // textBoxFileReadTime
+            // 
+            this.textBoxFileReadTime.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxFileReadTime.Location = new System.Drawing.Point(360, 54);
+            this.textBoxFileReadTime.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxFileReadTime.Name = "textBoxFileReadTime";
+            this.textBoxFileReadTime.ReadOnly = true;
+            this.textBoxFileReadTime.Size = new System.Drawing.Size(175, 15);
+            this.textBoxFileReadTime.TabIndex = 14;
+            this.textBoxFileReadTime.TextChanged += new System.EventHandler(this.textBoxFileReadTime_TextChanged);
+            // 
+            // textBoxFileReadCount
+            // 
+            this.textBoxFileReadCount.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxFileReadCount.Location = new System.Drawing.Point(452, 80);
+            this.textBoxFileReadCount.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxFileReadCount.Name = "textBoxFileReadCount";
+            this.textBoxFileReadCount.ReadOnly = true;
+            this.textBoxFileReadCount.Size = new System.Drawing.Size(109, 15);
+            this.textBoxFileReadCount.TabIndex = 15;
+            this.textBoxFileReadCount.TextChanged += new System.EventHandler(this.textBoxFileReadCount_TextChanged);
+            // 
+            // textBoxExactTime
+            // 
+            this.textBoxExactTime.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxExactTime.Location = new System.Drawing.Point(400, 157);
+            this.textBoxExactTime.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxExactTime.Name = "textBoxExactTime";
+            this.textBoxExactTime.ReadOnly = true;
+            this.textBoxExactTime.Size = new System.Drawing.Size(151, 15);
+            this.textBoxExactTime.TabIndex = 16;
+            // 
+            // listBoxResult
+            // 
+            this.listBoxResult.FormattingEnabled = true;
+            this.listBoxResult.ItemHeight = 16;
+            this.listBoxResult.Location = new System.Drawing.Point(184, 197);
+            this.listBoxResult.Margin = new System.Windows.Forms.Padding(4);
+            this.listBoxResult.Name = "listBoxResult";
+            this.listBoxResult.Size = new System.Drawing.Size(463, 260);
+            this.listBoxResult.TabIndex = 22;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(688, 551);
+            this.Controls.Add(this.listBoxResult);
+            this.Controls.Add(this.textBoxExactTime);
+            this.Controls.Add(this.textBoxFileReadCount);
+            this.Controls.Add(this.textBoxFileReadTime);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.textBoxFind);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxFind;
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBoxFileReadTime;
+        private System.Windows.Forms.TextBox textBoxFileReadCount;
+        private System.Windows.Forms.TextBox textBoxExactTime;
+        private System.Windows.Forms.ListBox listBoxResult;
+    }
+}
